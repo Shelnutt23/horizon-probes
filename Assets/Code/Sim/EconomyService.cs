1b7e019ac185c72e5a0dacff65fda789b4f169e3
Shelnutt23 <120670525+Shelnutt23@users.noreply.github.com>
2025-11-10 04:44:33 -0500

    Add starting resources to economy service

--- Assets/Code/Sim/EconomyService.cs
+++ Assets/Code/Sim/EconomyService.cs
@@ -1,10 +1,26 @@
 using UnityEngine;
 public class EconomyService : MonoBehaviour {
+  [SerializeField] int startingM = 25;
+  [SerializeField] int startingV = 15;
+  [SerializeField] int startingD = 20;
+  [SerializeField] float startingX = 0f;
+
   public int M { get; private set; }
   public int V { get; private set; }
   public int D { get; private set; }
   public float X { get; private set; }
-  void Awake(){ ServiceLocator.Register(this); }
+
+  void Awake(){
+    ResetToStartingResources();
+    ServiceLocator.Register(this);
+  }
+
+  void ResetToStartingResources(){
+    M = startingM;
+    V = startingV;
+    D = startingD;
+    X = startingX;
+  }
   public void Add(ResourceBundle r){ M += r.M; V += r.V; D += r.D; X += r.X; }
   public bool Spend(int m=0, int v=0, int d=0){
     if (M < m || V < v || D < d) return false;
